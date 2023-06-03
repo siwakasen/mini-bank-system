093e105cac7f0dcf27dbe4f4540448f0d84cc425
Panji <panjipusaka71@gmail.com>
2023-06-03 20:14:42 +0700

    idk

---
+++ src/dao/AdministratorDAO.java
@@ -0,0 +1,107 @@
+package dao;
+
+import connection.DbConnection;
+import model.*;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdministratorDAO {
+    DbConnection dbcon = new DbConnection();
+    private Connection con;
+    
+    public void insertAdministrator(Administrator a){
+        con = dbcon.makeConnection();
+        
+        String sql = "INSERT INTO administrators(administrator_id, username, password) "
+                + " VALUES("+a.getAdministrator_id()+", '"+a.getUsername()+"', '"+a.getPassword()+"')";
+        
+        System.out.println("Insert data administrator...");     
+        
+        try {
+            Statement statement = con.createStatement();
+            int rs = statement.executeUpdate(sql);
+            System.out.println("Success insert "+rs+" data administrator...");
+            statement.close();
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Error insert data administrator...");
+        }
+        
+        dbcon.closeConnection();
+    }
+    
+    public void deleteAdministrator(int id){
+        con = dbcon.makeConnection();
+        
+        String sql = "DELETE FROM administrators WHERE id = "+id+"";
+        
+        System.out.println("Deleting data administrator...");
+        
+        try {
+            Statement statement = con.createStatement();
+            int rs = statement.executeUpdate(sql);
+            System.out.println("Success deleting "+rs+" data administrator...");
+            statement.close();
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Error deleting data administrator...");
+        }
+        dbcon.closeConnection();
+    }
+    
+    public void updateAdministrator(Administrator a){
+        con = dbcon.makeConnection();
+        
+        String sql = "UPDATE administrator SET username = '"+a.getUsername()+"', password = '"+a.getPassword()+"'"
+                + " WHERE administrator_id = '"+a.getAdministrator_id()+"'";
+        
+        System.out.println("Updating data administrator...");
+        
+        try {
+            Statement statement = con.createStatement();
+            int rs = statement.executeUpdate(sql);
+            System.out.println("Success updating"+rs+" data administrator...");
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Error updating data administrator...");
+        }
+        dbcon.closeConnection();
+    }
+    
+    public List<Administrator> showListAdministrator(){
+        con = dbcon.makeConnection();
+        
+        String sql = "SELECT * FROM administrators";
+        
+        List<Administrator> list = new ArrayList<Administrator>();
+        System.out.println("Collecting data administrator...");
+        
+        try {
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            
+            if (rs!=null) {
+                while (rs.next()) {                    
+                    Administrator a = new Administrator(rs.getInt("administrator_id"),
+                            rs.getString("username"), rs.getString("password"));
+                    
+                    list.add(a);
+                }
+            }
+            
+            rs.close();
+            statement.close();
+            System.out.println("Success collecting data administrator...");
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Error colleting data administrator...");
+        }
+        dbcon.closeConnection();
+        return list;
+    }
+}
---
+++ src/model/Transfer.java
@@ -0,0 +1,57 @@
+package model;
+
+public class Transfer {
+    private String transfer_id,transfer_date;
+    private Account from_account_id,to_account_id;
+    private double amount;
+
+    public Transfer(String transfer_id, String transfer_date, Account from_account_id, Account to_account_id, double amount) {
+        this.transfer_id = transfer_id;
+        this.transfer_date = transfer_date;
+        this.from_account_id = from_account_id;
+        this.to_account_id = to_account_id;
+        this.amount = amount;
+    }
+
+    public String getTransfer_id() {
+        return transfer_id;
+    }
+
+    public void setTransfer_id(String transfer_id) {
+        this.transfer_id = transfer_id;
+    }
+
+    public String getTransfer_date() {
+        return transfer_date;
+    }
+
+    public void setTransfer_date(String transfer_date) {
+        this.transfer_date = transfer_date;
+    }
+
+    public Account getFrom_account_id() {
+        return from_account_id;
+    }
+
+    public void setFrom_account_id(Account from_account_id) {
+        this.from_account_id = from_account_id;
+    }
+
+    public Account getTo_account_id() {
+        return to_account_id;
+    }
+
+    public void setTo_account_id(Account to_account_id) {
+        this.to_account_id = to_account_id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+    
+    
+}
