82355454d94e6a7f8a9210169fd5006a47242f14
riksiprnm <maderiksi20@gmail.com>
2023-06-06 16:24:45 +0700

    make unfinish control

--- src/testing/testingDB.java
+++ src/testing/testingDB.java
@@ -1,42 +1,42 @@
 
 package testing;
-// import model.*;
-// import dao.*;
-// import connection.DbConnection;
-// import java.util.List;
+import model.*;
+import dao.*;
+import connection.DbConnection;
+import java.util.List;
 
 
 public class testingDB {
     
     public static void main(String[] args) {
-//        AdministratorDAO cDao = new AdministratorDAO();
-//        Administrator a = new Administrator(0, "admin4", "admin4");
-//        
-//        cDao.insertAdministrator(a);
-////        cDao.deleteAdministrator(2);
-////          cDao.updateAdministrator(a);
-//          List<Administrator> list = cDao.showListAdministrator();
-//          String dosenString = "";
-//            for (int i = 0; i < list.size(); i++) {
-//                dosenString = dosenString + list.get(i).showData()+ "\n";
-//            }
-//            
-//            System.out.println(dosenString);
+       AdministratorsDAO cDao = new AdministratorsDAO();
+//       Administrators a = new Administrators(4, "Riksi ", "123");
+//       
+//       cDao.insertAdministrator(a);
+//        cDao.deleteAdministrator(4);
+//          cDao.updateAdministrator(a);
+         List<Administrators> list = cDao.showListAdministrator();
+         String dosenString = "";
+           for (int i = 0; i < list.size(); i++) {
+               dosenString = dosenString + list.get(i).getUsername()+ list.get(i).getPassword()+ "\n";
+           }
+           
+           System.out.println(dosenString);
 
-        // EmployeeDAO eDao = new EmployeeDAO();
-        //     Employee e = new Employee(2, "1", "1", "1", "1", "2022/12/12", "2023-12-12", 1, "2", "");
+        // EmployeesDAO eDao = new EmployeesDAO();
+        //     Employees e = new Employees(2, "1", "1", "1", "1", "2022/12/12", "2023-12-12", 1, "2", "");
                 
-//            eDao.insertEmployee(e);
-//            eDao.deleteEmployee(1);
+        //    eDao.insertEmployee(e);
+        //    eDao.deleteEmployee(1);
 
-//            eDao.updateEmployee(e);
-        //               List<Employee> list = eDao.showListCustomer();
+        //    eDao.updateEmployee(e);
+        //               List<Employees> list = eDao.showListCustomer();
         //   String dosenString = "";
         //     for (int i = 0; i < list.size(); i++) {
         //         dosenString = dosenString + list.get(i).getFirst_name() +" | "+ list.get(i).getLast_name()+ "\n";
         //     }
             
-        //     System.out.println(dosenString);
+            System.out.println(dosenString);
     }
     
     
