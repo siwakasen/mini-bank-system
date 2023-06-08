ae6efd0dd729dc380242492f3b95c135a7c4777f
riksiprnm <maderiksi20@gmail.com>
2023-06-08 13:28:15 +0700

    resolve

--- src/control/CustomersControl.java
+++ src/control/CustomersControl.java
@@ -4,36 +4,43 @@
  */
 
 package control;
+
 import dao.CustomersDAO;
-import java.util.List;
 import model.Customers;
 import table.TableCustomers;
-/**
- * @author 
- * Made Riksi Purnama Sadnya Agung / 210711396
- * Pemrograman Berorientasi Obyek C
- */
+import java.util.List;
+
 public class CustomersControl {
-    private CustomersDAO customerDAO = new CustomersDAO();
-    public List<Customers> getAllCustomer(){
-        List<Customers> customers = customerDAO.showListCustomers();
-        return customers;
+    private CustomersDAO cDao = new CustomersDAO();
+    
+    public void insertCustomer(Customers c){
+        cDao.insertCustomers(c);
     }
-    public TableCustomers showCustomer(String query){
-        List<Customers> listCustomer = customerDAO.showCustomersBySearch(query);
-        TableCustomers tableCustomer = new TableCustomers(listCustomer);
-        return tableCustomer;
+    
+    public TableCustomers showCustomers(String query){
+        List<Customers> list = cDao.showCustomersBySearch(query);
+        TableCustomers tableCustomers = new TableCustomers(list);
+        
+        return tableCustomers;
     }
-
-    public void insertCustomer(Customers customer){
-        customerDAO.insertCustomers(customer);
+    
+    public int lastCostumer(){
+        List<Customers> list = cDao.showListCustomers();
+        int last = 0;
+        for (Customers customers : list) {
+            if (customers.getCustomer_id() > last) {
+                last = customers.getCustomer_id();
+            }
+        }
+        return last;
     }
-
-    public void updateCustomer(Customers customer){
-        customerDAO.updateCustomers(customer);
+    
+    public void updateCustomer(Customers c){
+        cDao.updateCustomers(c);
     }
-
-    public void deleteCustomer(int id){
-        customerDAO.deleteCustomers(id);
+    
+    public void deleteCustomer(int customer_id){
+        cDao.deleteCustomers(customer_id);
     }
+    
 }
